c2e49334090d99a940d361e99fe2a4aebf8e04e0
Kurt Smith <ksmith@enthought.com>
2012-03-04 20:49:27 -0600

    REF+ENH: Improve internal bitboard representation.
    Board position 'a1' corresponds to the lsb in a bitboard; 'h8'
    corresponds to the msb in a bitboard.
    
    Full test coverage of bitboards.py.

--- src/bitboards.py
+++ src/bitboards.py
@@ -6,72 +6,157 @@ Functions and classes for manipulating bitboards.
 #-----------------------------------------------------------------------------
 # Implementation details:
 #
-# The most sig. bit of a bitboard corresponds to square a8 of an Arimaa board.
+# The most sig. bit of a bitboard corresponds to square h8 of an Arimaa board.
+# The least sig. bit of a bitboard corresponds to squard a1 of an Arimaa board.
 # 
 # North corresponds to adding 8 to an index (left shifting a bb by 8).
 # South corresponds to subtracting 8 from an index(right shifting a bb by 8).
-# East corresponds to subtracting 1 from an index (right shifting a bb by 1).
-# West corresponds to adding 1 to an index (left shifting a bb by 1).
+# East corresponds to adding 1 to an index (right shifting a bb by 1).
+# West corresponds to subtracting 1 from an index (left shifting a bb by 1).
 #
 #         a  b  c  d  e  f  g  h
 #      +-------------------------+
-#     8| 63 62 61 60 59 58 57 56 |8
-#     7| 55                   48 |7
-#     6| 47                   40 |6
-#     5| 39                   32 |5
-#     4| 31                   24 |4
-#     3| 23                   16 |3
-#     2| 15                    8 |2
-#     1|  7  6  5  4  3  2  1  0 |1
+#     8| 56 57 58 59 60 61 62 63 |8
+#     7| 48                   55 |7
+#     6| 40                   47 |6
+#     5| 32                   39 |5
+#     4| 24                   31 |4
+#     3| 16                   23 |3
+#     2|  8                   15 |2
+#     1|  0  1  2  3  4  5  6  7 |1
 #      +-------------------------+
 #         a  b  c  d  e  f  g  h
 #
 #-----------------------------------------------------------------------------
 
-def move_north(posn, board):
-    ''' Move piece at posn one step north.
+RANK8 = 0
+for i in range(56, 64):
+    RANK8 |= (1<<i)
+
+RANK1 = 0
+for i in range(0, 8):
+    RANK1 |= (1<<i)
+
+FILEH = 0
+for i in range(7, 64, 8):
+    FILEH |= (1<<i)
+
+FILEA = 0
+for i in range(0, 64, 8):
+    FILEA |= (1<<i)
+
+_dir_to_edge_and_shift = {
+        'N': (RANK8, 8),
+        'S': (RANK1, -8),
+        'E': (FILEH, 1),
+        'W': (FILEA, -1),
+        }
+def _move_dir(board, dir):
+    EDGE, shift = _dir_to_edge_and_shift[dir]
+    moveable = board & ~EDGE
+    if shift > 0:
+        return (board & EDGE) | (moveable << shift)
+    else:
+        return (board & EDGE) | (moveable >> -shift)
+
+def move_north(board):
+    ''' Move every piece on board north.
+    Pieces at top of board will stay put.
     '''
-    raise Exception("test me!")
-    assert (board & posn)
-    return (board ^ posn) | (board | (posn << 8))
+    return _move_dir(board, 'N')
+
+def move_south(board):
+    return _move_dir(board, 'S')
+
+def move_east(board):
+    return _move_dir(board, 'E')
+
+def move_west(board):
+    return _move_dir(board, 'W')
+
+def _reverse_str(s):
+    return ''.join(reversed(s))
 
 def bb_repr(board, N=8):
     ''' Returns a nice string representation of a bitboard.
     '''
     res = []
     ctr = N
-    fmt = ('{'+':0>{N}b'.format(N=N)+'}').format
+    fmt = lambda s: _reverse_str(('{'+':0>{N}b'.format(N=N)+'}').format(s))
     least_sig_bits = (1 << N) - 1
     while ctr:
         res.append(fmt(board & least_sig_bits))
-        board = board >> N
+        board = (board >> N)
         ctr -= 1
     return '\n'.join(reversed(res))
 
 def board_to_posns(board):
     ''' Given a bitboard, returns a list of 1D board indexes.
     '''
-    return [i-1 for i in range(64) if (board & (1<<i))]
+    return [i for i in range(64) if (board & (1<<i))]
+
+def _char_state_iter(ch_board):
+    '''
+    Iterates through the ch_board in ascending bb index.
+
+    ch_board is given in the order a8-h8, a7-h7, ..., a1-h1.
+    '''
+
+    rows = reversed([ch_board[i*8:(i+1)*8] for i in range(8)])
+    res = []
+    for row in rows:
+        res.extend(row)
+    return res
 
 def bboards_from_char_state(ch_board):
     ''' Given a standard notation Arimaa board, returns a dictionary of 
     <piece label> => <position bboard> entries.
+
+    Parameters
+    ----------
+    ch_board -- str containing a valid Arimaa position.
+        Squares are given in the order a8-h8, a7-h7, ..., a1-h1.
+
+    Returns
+    -------
+    Dictionary mapping from either piece label ('rcdhme') or color ('wb') to
+    bitboard representing position of resp. type.
+
     '''
     piece_posns = {}
-    for i, ch in enumerate(reversed(ch_board)):
+    for i, ch in enumerate(_char_state_iter(ch_board)):
         if ch not in ('rcdhmeRCDHME'):
             continue
-        board_idx = 1<<i
-        piece_posns[ch] = piece_posns.get(ch, 0) | board_idx
+        board_idx = (1 << i)
+        lch = ch.lower()
+        piece_posns[lch] = piece_posns.get(lch, 0) | board_idx
+        if ch.islower():
+            color = 'b'
+        else:
+            color = 'w'
+        piece_posns[color] = piece_posns.get(color, 0) | board_idx
     return piece_posns
 
-def char_state_from_bboards(bboards):
+def char_state_from_bboards(bboards, empty=' '):
     ''' Given a dictionary of <piece label> => <position bboard> entries,
     returns a standard notation Arimaa board.
     '''
-    ch_board = ['-'] * 64
-    for piece, board in bboards.items():
-        for idx in board_to_posns(board):
-            assert ch_board[idx] == '-'
-            ch_board[idx] = piece
+    ch_board = [empty] * 64
+    wps = bboards.get('w', 0) # white / gold pieces
+    bps = bboards.get('b', 0) # black / silver pieces
+    for piece in 'rcdhme':
+        # get the white / black piece for the piece type.
+        bbb = bboards.get(piece, 0) & bps
+        wbb = bboards.get(piece, 0) & wps
+        # iterate over (piece label, piece locations) tuples.
+        for piece, board in ((piece.lower(), bbb),
+                             (piece.upper(), wbb)):
+            for idx in board_to_posns(board):
+                # FIXME: refactor these lines
+                # Necessary to correct the column index.
+                row, col = divmod(idx, 8) 
+                col = -col + 7
+                idx = 8 * row + col
+                assert ch_board[idx] == empty
+                ch_board[idx] = piece
     return ''.join(reversed(ch_board))
--- src/test_bitboards.py
+++ src/test_bitboards.py
@@ -1,7 +1,53 @@
-from bitboards import bb_repr
+import unittest
+import bitboards as bb
 
-def test_bb_repr():
-    assert bb_repr((1<<64) - 1) == '''\
+
+class TestBitboards(unittest.TestCase):
+
+    def setUp(self):
+        board = 0
+        for i in range(0, 64, 9):
+            board |= (1<<i)
+        self.diag_board = board
+
+    def test_move_north(self):
+        self.assertEquals(bb.move_north(bb.RANK8), bb.RANK8)
+
+        nboard = 0
+        for i in range(8, 64, 9):
+            nboard |= (1<<i)
+        nboard |= (1<<63)
+        self.assertEquals(bb.move_north(self.diag_board), nboard)
+
+    def test_move_south(self):
+        self.assertEquals(bb.move_south(bb.RANK1), bb.RANK1)
+
+        sboard = 0
+        for i in range(1, 64, 9):
+            sboard |= (1<<i)
+        sboard |= 1
+        self.assertEquals(bb.move_south(self.diag_board), sboard)
+
+    def test_move_east(self):
+        self.assertEquals(bb.move_east(bb.FILEH), bb.FILEH)
+
+        eboard = 0
+        for i in range(1, 64, 9):
+            eboard |= (1<<i)
+        eboard |= (1<<63)
+        self.assertEquals(bb.move_east(self.diag_board), eboard)
+
+    def test_move_west(self):
+        self.assertEquals(bb.move_west(bb.FILEA), bb.FILEA)
+
+        wboard = 0
+        for i in range(8, 64, 9):
+            wboard |= (1<<i)
+        wboard |= (1<<0)
+        self.assertEquals(bb.move_west(self.diag_board), wboard)
+
+    def test_bb_repr(self):
+        self.assertEquals(bb.bb_repr((1<<64) - 1), '''\
 11111111
 11111111
 11111111
@@ -10,8 +56,8 @@ def test_bb_repr():
 11111111
 11111111
 11111111\
-'''
-    assert bb_repr(0) == '''\
+''')
+        self.assertEquals(bb.bb_repr(0), '''\
 00000000
 00000000
 00000000
@@ -20,4 +66,42 @@ def test_bb_repr():
 00000000
 00000000
 00000000\
-'''
+''')
+        self.assertEquals(bb.bb_repr(self.diag_board), '''\
+00000001
+00000010
+00000100
+00001000
+00010000
+00100000
+01000000
+10000000\
+''')
+
+
+    def test_board_to_positions(self):
+        bd = (1)
+        self.assertEquals(bb.board_to_posns(bd), [0])
+        for i in range(64):
+            self.assertEquals(bb.board_to_posns((1<<i)), [i])
+            if not i:
+                continue
+            for j in range(i):
+                self.assertEquals(bb.board_to_posns((1<<i) | (1<<j)), [j, i])
+                
+
+    def test_bb_to_from_ch_state(self):
+        # corner case: empty board.
+        ch_state = ' ' * 64
+        psns = bb.bboards_from_char_state(ch_state)
+        self.assertFalse(psns)
+        self.assertEquals(ch_state, bb.char_state_from_bboards(psns))
+
+        # test all piece types.
+        ch_state = [' '] * 64
+        pieces = 'rcdhmeRCDHME'
+        for idx, p in enumerate(pieces, 10):
+            ch_state[idx] = p
+        ch_state = ''.join(ch_state)
+        psns = bb.bboards_from_char_state(ch_state)
+        self.assertEquals(ch_state, bb.char_state_from_bboards(psns))
