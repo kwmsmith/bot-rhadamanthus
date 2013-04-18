ceda473ae6c774b3724d0e47a7315bbabb160065
Kurt Smith <ksmith@enthought.com>
2013-04-17 21:25:13 -0500

    ENH: Allow testing of generate_moves() performance.

---
+++ src/gen_move_perf.cpp
@@ -0,0 +1,28 @@
+#include "move.h"
+#include <iostream>
+
+typedef std::vector<MovePtr>::const_iterator move_it;
+
+int main(int argc, char **argv)
+{
+    std::vector<MovePtr> moves;
+    GameState gs;
+    std::string ss;
+    ss = 
+        "..RRRRm."
+        "R.C...ER"
+        "..x.ex.r"
+        "DdRr...h"
+        ".....MHr"
+        "..x.Hxd."
+        "r.r.rr.."
+        ".rc..c.."
+        ;
+    gs.clear();
+    moves.clear();
+    gamestate_from_string(ss, &gs);
+    generate_moves(gs, W, &moves);
+    // EXPECT_EQ(moves.size(), 204785);
+    
+    return 0;
+}
