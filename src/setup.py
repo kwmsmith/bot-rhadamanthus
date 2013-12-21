2f771cc6b9a454c3f6524c1cc4cded6ae4d026fa
Kurt Smith <ksmith@enthought.com>
2013-12-20 21:22:58 -0600

    File moving and renaming for Python stuff.

--- src/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import Extension, setup
-import os
-
-os.system("sip -c . board.sip")
-
-ext = Extension("board", 
-                sources=["board.cpp", "sipboardBoard.cpp", "sipboardcmodule.cpp"])
-
-setup(name="board", ext_modules=[ext])
